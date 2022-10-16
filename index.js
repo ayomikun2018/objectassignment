229445257d61629b44000efe8348fbc4ea4c387a
ayomikun2018 <ayomikunesther12@gmail.com>
2022-10-16 19:16:30 +0100

    updates

--- index.js
+++ index.js
@@ -1,71 +1,72 @@
 // // Exercise 1
-// let  address = {
-//   street: 'lekki',
-//   city: 'lagos',
-//   zipCode : 2242
-// }
+let  address = {
+  street: 'lekki',
+  city: 'lagos',
+  zipCode : 2242
+}
 
-// function showAddress (address){
-//   for (let key in address)
-//   console.log(key,address[key])
-// }
-// showAddress(address);
+function showAddress (address){
+  for (let key in address)
+  console.log(key,address[key])
+}
+showAddress(address);
 
 // Exercise 2
 //factory functions
-// function createShowAddress (){
-//   return{
-//     street: 'lekki',
-//     city: 'lagos',
-//     zipCode : 2242
-//   }
-// }
-// const address =  createShowAddress()
-// console.log(address)
+function createShowAddress (){
+  return{
+    street: 'lekki',
+    city: 'lagos',
+    zipCode : 2242
+  }
+}
+const address =  createShowAddress()
+console.log(address)
 // constructor functions 
 
-// function ShowAddress(address){
-//   this.street = 'lekki'
-//   this.city = 'lagos'
-//   this.zipCode = 2242
-// } 
-//   const address = new ShowAddress()
-//      console.log(address)
+function ShowAddress(address){
+  this.street = 'lekki'
+  this.city = 'lagos'
+  this.zipCode = 2242
+} 
+  const address = new ShowAddress()
+     console.log(address)
 
 // Exercise 3
-// create 2 address object-(using constructor func)
-//create 2 function areEqual and areSame
-//let areEqual pass the 2 objects  to check if their properties are equal return true, else false
-//let areSame  check if both objects have the same location in memory
+const address = {
+  street : 'lekki',
+  city :'lagos',
+  zipCode : 2242
+}
+function ShowAddress(address){
+    this.address = address;
+    return this.address
+  } 
+  const newAddress= new ShowAddress (address)
 
-// function CreateAddress(address){
-//     this.address = address;
-//     return this.address
-//   } 
-//   const newAddress= new CreateAddress (address)
+  const address1 = new ShowAddress(address)
+  const address2 = new ShowAddress(address)
 
-//   const address1 = new CreateAddress(address)
-//   const address2 = new CreateAddress(address)
+function areEqual (a,b){
+  for (let key in address)
+  if (a===b) console.log(true); else console.log(false);
+}
+areEqual (address1, address2)
 
-// function areEqual (a,b){
-//   for (let key in address)
-//   if (a===b) console.log(true); else console.log(false);
-// }
-// areEqual (address1, address2)
+function areSame(a,b){
+if (a=== address && b=== address2) console.log(true); else console.log(false)
+}
+areSame(address1,address2)
 
-// function areSame(a,b){
-// if (a=== address && b=== address2) console.log(true); else console.log(false)
-// }
-// areSame(address1,address2)
 // Exercise 4
-// let blogPost = {
-//   title: 'Woman',
-//   body:'A woman is strong by all means',
-//   author:'mikun',
-//   views:500,
-//   isLive: true
-// }
-// console.log(blogPost);
+let blogPost = {
+  title: 'Woman',
+  body:'A woman is strong by all means',
+  author:'mikun',
+  views:500,
+  isLive: true
+}
+console.log(blogPost);
 
 // // Exercise 5
 let programming = {
